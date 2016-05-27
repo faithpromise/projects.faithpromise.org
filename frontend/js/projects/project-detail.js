dd232af961575d65afd545ed73af448e8986d7a9
Brad Roberts <brad@bradroberts.net>
2016-05-27 12:42:37 -0400

    Updates to project detail directive

--- frontend/js/projects/project-detail.js
+++ frontend/js/projects/project-detail.js
@@ -1,4 +1,4 @@
-(function (module) {
+(function (module, angular) {
     'use strict';
 
     module.directive('projectDetail', directive);
@@ -14,20 +14,81 @@
         };
     }
 
-    Controller.$inject = ['$routeParams', 'projectsService'];
+    Controller.$inject = ['$scope', '$timeout', '$routeParams', '$location', 'projectsService', 'tasksService', 'pike'];
 
-    function Controller($routeParams, projectsService) {
+    function Controller($scope, $timeout, $routeParams, $location, projectsService, tasksService, pike) {
 
-        var vm = this;
+        var vm                    = this,
+            project_notes_timeout = null;
+
+        vm.route_action      = pike.bind($scope, 'project', on_route_change);
+        vm.new_task          = {};
+        vm.open_task         = open_task;
+        vm.on_task_closed    = on_task_closed;
+        vm.on_task_submitted = on_task_submitted;
+        vm.on_task_added     = on_task_added;
 
         init();
 
         function init() {
-            projectsService.find($routeParams.id).then(function (result) {
+            fetchProject();
+            fetchTasks();
+        }
+
+        function fetchProject() {
+            return projectsService.find($routeParams.id).then(function (result) {
                 vm.project = result.data.data;
             });
         }
 
+        function fetchTasks() {
+            vm.tasks = null;
+            return tasksService.byProject($routeParams.id).then(function (result) {
+                vm.tasks = result.data.data;
+            });
+        }
+
+        function save_project_notes() {
+            var data                = { notes: vm.project.notes };
+            vm.project_notes_saving = true;
+            projectsService.update($routeParams.id, data).then(function () {
+                vm.project_notes_saving = false;
+            });
+        }
+
+        function auto_save_project_notes(newVal, oldVal) {
+            if (typeof newVal !== 'undefined' && typeof oldVal !== 'undefined' && newVal != oldVal) {
+                if (project_notes_timeout) {
+                    $timeout.cancel(project_notes_timeout)
+                }
+                project_notes_timeout = $timeout(save_project_notes, 1000);  // 1000 = 1 second
+            }
+        }
+
+        function on_task_submitted() {
+            vm.tasks = null;
+        }
+
+        function on_task_added() {
+            fetchTasks();
+        }
+
+        function open_task(task) {
+            vm.selected_task = task;
+            $location.url('/projects/' + vm.project.id + '/tasks/' + task.id);
+        }
+
+        function on_task_closed() {
+            $location.url('/projects/' + vm.project.id);
+            fetchTasks();
+        }
+
+        function on_route_change(event, next_action) {
+            vm.route_action = next_action;
+        }
+
+        $scope.$watch('vm.project.notes', auto_save_project_notes);
+
     }
 
-})(angular.module('app'));
+})(angular.module('app'), angular);
