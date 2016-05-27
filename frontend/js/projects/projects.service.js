ddce683ad54556b82f3b02b36a259d788aa4a35f
Brad Roberts <brad@bradroberts.net>
2016-05-27 12:39:36 -0400

    Added projects service

--- frontend/js/projects/projects.service.js
+++ frontend/js/projects/projects.service.js
@@ -15,6 +15,14 @@
 
             find: function(id) {
                 return $http.get('/api/projects/' + id);
+            },
+
+            create: function (project) {
+                return $http.post('/api/projects', project);
+            },
+
+            update: function (id, project) {
+                return $http.put('/api/projects/' + id, project);
             }
 
         };
