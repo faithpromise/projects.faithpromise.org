c09c0df62580fca73c19848fa3d6581c53fa66a3
Brad Roberts <brad@bradroberts.net>
2016-05-27 12:42:16 -0400

    Added event detail directive

---
+++ frontend/js/events/event-detail.js
@@ -0,0 +1,33 @@
+(function (module) {
+    'use strict';
+
+    module.directive('eventDetail', directive);
+
+    function directive() {
+        return {
+            templateUrl:      '/build/js/events/event-detail.html',
+            restrict:         'E',
+            controller:       Controller,
+            controllerAs:     'vm',
+            bindToController: true,
+            scope:            {}
+        };
+    }
+
+    Controller.$inject = ['$routeParams', 'eventsService'];
+
+    function Controller($routeParams, eventsService) {
+
+        var vm = this;
+
+        init();
+
+        function init() {
+            eventsService.find($routeParams.id).then(function (result) {
+                vm.event = result.data.data;
+            });
+        }
+
+    }
+
+})(angular.module('app'));
