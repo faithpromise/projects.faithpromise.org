c206d899f9cd6705ad9aacffb0e41eb00d08b797
Brad Roberts <brad@bradroberts.net>
2016-06-13 15:01:21 -0400

    Added on-escape directive

---
+++ frontend/js/app/on-escape.js
@@ -0,0 +1,46 @@
+(function (module) {
+    'use strict';
+
+    var ignore_elements = ['INPUT', 'TEXTAREA', 'SELECT'];
+
+    module.directive('onEscape', Directive);
+
+    Directive.$inject = ['$document'];
+
+    function Directive($document) {
+        return {
+            restrict: 'A',
+            link:     function (scope, element, attrs) {
+
+                var escaped = false;
+
+                function bind_esc(event) {
+
+                    if (ignore_elements.indexOf(event.target.tagName) < 0) {
+
+                        if (escaped) { return; }
+                        escaped = true;
+
+                        if (event.which === 27) {
+                            scope.$apply(function () {
+                                scope.$eval(attrs.onEscape);
+                            });
+                            event.preventDefault();
+                        }
+
+                    }
+
+                }
+
+                $document.on("keydown keypress keyup", bind_esc);
+
+                scope.$on('$destroy', function () {
+                    console.log('destroying');
+                    $document.off("keydown keypress keyup", bind_esc);
+                });
+
+            }
+        };
+    }
+
+})(angular.module('app'), angular);
