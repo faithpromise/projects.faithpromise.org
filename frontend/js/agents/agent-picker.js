a3470b9279fbd69268e84fa5cd713f181ba7e3fc
Brad Roberts <brad@bradroberts.net>
2016-06-17 15:22:22 -0400

    Added agent picker

---
+++ frontend/js/agents/agent-picker.js
@@ -0,0 +1,49 @@
+(function (module) {
+    'use strict';
+
+    module.directive('agentPicker', directive);
+
+    function directive() {
+        return {
+            template:         '<select class="CustomSelect-control" ng-model="vm.agent" ng-options="agent.name for agent in vm.agents" ng-change="vm.on_change_handler()" tabindex="2"></select>',
+            restrict:         'E',
+            controller:       Controller,
+            controllerAs:     'vm',
+            bindToController: {
+                agent_id: '@',
+                onChange: '&?'
+            },
+            scope:            true
+        };
+    }
+
+    Controller.$inject = ['agentsService'];
+
+    function Controller(agentsService) {
+
+        var vm               = this;
+        vm.on_change_handler = on_change_handler;
+
+        init();
+
+        function init() {
+            fetch_agents();
+        }
+
+        function fetch_agents() {
+            agentsService.all().then(function (result) {
+                vm.agents = result.data.data;
+            });
+        }
+
+        function on_change_handler() {
+            if (this.onChange) {
+                this.onChange({
+                    agent: vm.agent
+                });
+            }
+        }
+
+    }
+
+})(angular.module('app'));
