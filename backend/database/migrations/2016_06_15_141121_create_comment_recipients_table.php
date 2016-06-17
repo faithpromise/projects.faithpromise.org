8af6b89d597e40de84c7d378c996337b1f7e566b
Brad Roberts <brad@bradroberts.net>
2016-06-17 15:22:30 -0400

    Added migration

---
+++ backend/database/migrations/2016_06_15_141121_create_comment_recipients_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentRecipientsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('comment_recipients', function (Blueprint $table) {
+            $table->integer('comment_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('comment_recipients');
+    }
+}
