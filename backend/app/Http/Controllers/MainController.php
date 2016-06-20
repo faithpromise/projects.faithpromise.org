c189e35e28ca1c59ab50c6cf593257398575b0fd
Brad Roberts <brad@bradroberts.net>
2016-06-20 13:26:22 -0400

    Added TODO

--- backend/app/Http/Controllers/MainController.php
+++ backend/app/Http/Controllers/MainController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Agent;
+use App\Services\TimelineBuilder;
 
 class MainController extends Controller {
 
     public function index() {
 
-        $agents = Agent::with('tasks')->get();
-
-        dd($agents);
+        // TODO: Do not need to re-generate timeline on each request. This is just for dev
+        $agents = Agent::with('tasks.project.event')->get();
+        foreach ($agents as $agent) {
+            TimelineBuilder::createTimeline($agent);
+        }
 
         return view('welcome');
     }
