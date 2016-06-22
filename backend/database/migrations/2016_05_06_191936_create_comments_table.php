bae172021c5094139b62b43c7fbdb99d0ce0495a
Brad Roberts <brad@bradroberts.net>
2016-06-22 10:53:11 -0400

    Removed unused field

--- backend/database/migrations/2016_05_06_191936_create_comments_table.php
+++ backend/database/migrations/2016_05_06_191936_create_comments_table.php
@@ -17,7 +17,6 @@ class CreateCommentsTable extends Migration {
             $table->integer('project_id')->unsigned()->nullable();
             $table->integer('user_id')->unsigned();
             $table->string('type', 100)->nullable(); // estimate, mockup, proof
-            $table->date('approved_at')->nullable();
             $table->text('body');
             $table->dateTime('sent_at')->nullable();
             $table->timestamps();
