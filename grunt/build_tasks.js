c7cfac1a3e5b427a507ce159ad7aa0e6e1ca71bf
Brad Roberts <brad@bradroberts.net>
2016-06-23 11:16:18 -0400

    Fixed deploy task

--- grunt/build_tasks.js
+++ grunt/build_tasks.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build_app', ['uglify:app', 'copy_files', 'css']);
 
-    grunt.registerTask('build_release', ['clean:build', 'clean:release', 'build_app', 'copy:release_backend', 'copy:release_frontend', 'removelogging:website']);
+    grunt.registerTask('build_release', ['clean:build', 'clean:release', 'build_app', 'copy:release_backend', 'copy:release_frontend', 'removelogging:app']);
 
     grunt.registerTask('deploy', ['build_release', 'git_deploy']);
 
