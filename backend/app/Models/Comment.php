342dcf147cfb22354c348122594eadc329613989
Brad Roberts <brad@bradroberts.net>
2016-06-23 08:37:39 -0400

    Comment getters and setters

--- backend/app/Models/Comment.php
+++ backend/app/Models/Comment.php
@@ -33,4 +33,62 @@ class Comment extends Model {
         return $this->hasMany(Attachment::class);
     }
 
+    /**
+     * Getters and setters
+     */
+
+    public function setUserId($param) {
+        $this->{'user_id'} = $param;
+        return $this;
+    }
+
+    public function getUserId() {
+        return $this->{'user_id'};
+    }
+
+    public function setEventId($param) {
+        $this->{'event_id'} = $param;
+        return $this;
+    }
+
+    public function getEventId() {
+        return $this->{'event_id'};
+    }
+
+    public function setProjectId($param) {
+        $this->{'project_id'} = $param;
+        return $this;
+    }
+
+    public function getProjectId() {
+        return $this->{'project_id'};
+    }
+
+    public function setType($param) {
+        $this->{'type'} = $param;
+        return $this;
+    }
+
+    public function getType() {
+        return $this->{'type'};
+    }
+
+    public function setBody($param) {
+        $this->{'body'} = $param;
+        return $this;
+    }
+
+    public function getBody() {
+        return $this->{'body'};
+    }
+
+    public function setSentAt($param) {
+        $this->{'sent_at'} = $param;
+        return $this;
+    }
+
+    public function getSentAt() {
+        return $this->{'sent_at'};
+    }
+
 }
